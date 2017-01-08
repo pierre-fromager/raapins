87ad6d1faadb98793ac982dec449e1108d326ea1
pierre-fromager <pf@pier-infor.fr>
2017-01-08 15:01:11 +0100

    refacto/lib dater

--- src/lib/dater.js
+++ src/lib/dater.js
@@ -3,15 +3,15 @@
  * 
  */
 const dater = {
-    dsep : '-',
-    tsep : ':',
-    zop : ' ',    
+    ds : '-',
+    ts : ':',
+    zs : ' ',    
     /**
      * leftPad
      * 
      * @param {Int} v
      * @param {String} l
-     * @returns {nm$_dater.exports.leftPad.l|dater.leftPad.l|@var;l|String}
+     * @returns {String}
      */
     leftPad(v, l) {
         l = (l) ? l : '00';
@@ -25,12 +25,12 @@ const dater = {
     getDateTime() {
         var date = new Date();
         return date.getFullYear() 
-            + this.dsep + this.leftPad(date.getMonth() + 1) 
-            + this.dsep + this.leftPad(date.getDate()) 
-            + this.zop + this.leftPad(date.getHours()) 
-            + this.tsep + this.leftPad(date.getMinutes()) 
-            + this.tsep + this.leftPad(date.getSeconds()) 
-            + this.tsep + this.leftPad(date.getMilliseconds(), '000');
+            + this.ds + this.leftPad(date.getMonth() + 1) 
+            + this.ds + this.leftPad(date.getDate()) 
+            + this.zs + this.leftPad(date.getHours()) 
+            + this.ts + this.leftPad(date.getMinutes()) 
+            + this.ts + this.leftPad(date.getSeconds()) 
+            + this.ts + this.leftPad(date.getMilliseconds(), '000');
     }
 }
 
