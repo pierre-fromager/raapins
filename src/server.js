c22c701bbbd46d73a8b04485134839c5ac8ce309
pierre-fromager <pf@pier-infor.fr>
2017-01-11 19:57:27 +0100

    feature/server display connections status & use color

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+const profiler = require('./lib/profiler.js');
 const config = require('./lib/config.js');
+const color = require('./lib/color.js');
+const dater = require('./lib/dater.js');
 const http = require('http');
 const server = http.createServer()
     .listen(config.server.port, config.server.address)
@@ -8,21 +11,28 @@ const qs = require('querystring');
 const cors = require('./lib/cors.js');
 const controller = require('./lib/controller.js');
 controller.setMaxListeners(config.process.maxlisteners);
+profiler.add('starting');
 
 server.on('request', (req, res) => {
     
+    profiler.add('request');
+    
     server.getConnections(function(error, count) {
-        console.log('connection count : ' + count);
+        let counter = color.get(' >' + count + '< ', color.codes.yellow, color.codes.blue);
+        process.stdout.write(
+            dater.getDateTime() + counter + 'requests ' + "\r"
+        );
     });
     
     let reqRawData = '';
     
     req.on('data', (rawData) => {
+        profiler.add('requestdata');
         reqRawData += rawData;
     });
         
     req.on('end', () => {
-        
+        profiler.add('requestend');
         controller.once('data', (data) => {
             res.writeHead(200, cors.getHeaders());
             res.end(JSON.stringify(data));
@@ -34,7 +44,12 @@ server.on('request', (req, res) => {
         });
     
         controller.handle(req, res, qs.parse(reqRawData));
+        profiler.add('requestendfinished');
     });
 });
-
-console.log(config.getServerStartMessage());
+var startMessage = color.get(
+    config.getServerStartMessage()
+    , color.codes.white
+    , color.codes.black
+);
+console.log(startMessage);
