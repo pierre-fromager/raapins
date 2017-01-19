7d45009dbe77c3e147092f8ea502b49b782f8792
pierre-fromager <pf@pier-infor.fr>
2017-01-19 08:40:12 +0100

    feature/router : fix regexp

---
+++ src/lib/router.js
@@ -0,0 +1,49 @@
+const router = {
+    separator : '/',
+    empty: '',
+    routes: [],
+    root: this.separator,
+    server: null,
+    match: false,
+    req: null,
+    uri : null,
+    method: null,
+    config: function (options) {
+        this.root = (options && options.root) 
+            ? this.separator + this.clearUri(options.root) + this.separator
+            : this.separator;
+        this.sever = (options && options.server) 
+            ? options.server
+            : this.sever;
+        return this;
+    },
+    clearUri: function (path) {
+        return path.toString().replace(/\/$/, this.empty).replace(/^\//, this.empty);
+    },
+    add: function (method, re, handler) {
+        if (typeof re == 'function') {
+            handler = re;
+            re = this.empty;
+        }
+        this.routes.push({method: method, re: re, handler: handler});
+        return this;
+    },
+    check: function (req) {
+        this.req = req;
+        this.uri = this.clearUri(req.url);
+        this.method = req.method;
+        let methodMatch = false;
+        for (var i = 0; i < this.routes.length; i++) {
+            methodMatch = req.method == this.routes[i].method;
+            this.match = this.uri.match(this.routes[i].re);
+            if (this.match && methodMatch) {
+                this.match.shift();
+                this.routes[i].handler.apply({}, this.match);
+                return this;
+            }
+        }
+        return this;
+    }
+}
+
+module.exports = router;
