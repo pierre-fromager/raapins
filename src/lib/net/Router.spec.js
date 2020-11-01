33de08f9b8f151ebca9f84b685ff8d316fcc8b9e
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2020-11-01 17:26:38 +0100

    test : Router init

--- src/lib/net/Router.spec.js
+++ src/lib/net/Router.spec.js
@@ -3,15 +3,32 @@ import chaiPkg from 'chai';
 import mochaPkg from 'mocha';
 import { Router } from './Router.js';
 
-const { expect,assert } = chaiPkg;
-const { describe, it} = mochaPkg;
+const { expect, assert } = chaiPkg;
+const { describe, it } = mochaPkg;
+
+let routerInst;
+
+const instSuccMsg = 'Router is an instance of Router';
 
 describe('test router', () => {
 
-    it('router config', () => {
-        const r = new Router();
-        expect(r.config({ root: '/' })).to.be.an.instanceof(Object);
-        assert.instanceOf(r, Router, 'chai is an instance of tea');
+    beforeEach(() => {
+        routerInst = new Router();
+    })
+
+    /*
+    afterEach(function (done) {
+        routerInst = null;
+    });*/
+
+    it('Router instance', () => {
+        expect(routerInst).to.be.an.instanceof(Router);
+        assert.instanceOf(routerInst, Router, instSuccMsg);
+    });
+
+    it('Router config', () => {
+        expect(routerInst.config({ root: '/' })).to.be.an.instanceof(Router);
+        assert.instanceOf(routerInst, Router, instSuccMsg);
     });
 
 });
